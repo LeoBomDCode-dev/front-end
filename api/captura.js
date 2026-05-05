cda9b400c293fbb1b1cb6c85dc31af38c76ac6f5
LeoBomDCode-dev <leonardo.t.cruz6@aluno.senai.br>
2026-05-05 16:49:09 -0300

    Meu Site Flexivel

--- api/captura.js
+++ api/captura.js
@@ -1,7 +1,7 @@
 const video = document.getElementById("cameraFeed");
 const canvas = document.getElementById("capturedImage");
-const button = document.getElementById("captureButton");
 const contexto = canvas.getContext("2d");
+const button = document.getElementById("captureButton");
 
 navigator.mediaDevices.getUserMedia({ video: true})
     .then(stream => {
@@ -15,22 +15,31 @@ navigator.mediaDevices.getUserMedia({ video: true})
    button.addEventListener("click", () => {
         //desenhar o quadro atual do vídeo na área do canvas
         contexto.drawImage(video, 0, 0, canvas.width, canvas.height);
-
         //obter a imagem como um url de dados
-        const imageDataURL = canvas.toDataURl("image/png");
+        const imageDataURL = canvas.toDataURL("image/png");
         //enviar imagem para um servidor
         enviarImagemParaServidor(imageDataURL);
    })
 
    //função para enviar imagem para um servidor
-function enviarImagemParaServidor(imageDataURL){};
+function enviarImagemParaServidor(imageDataURL){;
    //simular o envio para um servidor
    console.log("Enviando imagem para o servidor...");
 
-   fetch("/", {
-    method: "POST",
-    body: JSON.stringify({image: imageDataURL}),
-    headers: {
-        "Content-type": "application/json"    
+    const base64String = imageDataURL.split(',')[1];   
+
+   fetch("http://DOP3080-1247456:8000/images", {
+        method: "POST",
+        body: JSON.stringify({image: base64String, mime_type: 'image/png'}),
+        headers: {
+            "Content-type": "application/json"    
       }
    })
+    .then( resposta => resposta.json())
+    .then(dados => {
+        console.log("Resposta do servidor: ", dados);
+    })
+    .catch( erro => {
+        console.error("Erro ao enviar a imagem: ", erro);
+    })
+}
